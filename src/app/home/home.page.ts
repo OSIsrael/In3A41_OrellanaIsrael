e9262a8a5fe94a451ad9741699186e4590157da1
OSIsrael <israelos067@gmail.com>
2024-02-22 16:55:56 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { Firestore, setDoc } from '@angular/fire/firestore';
+
+import { doc } from 'firebase/firestore';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  condicion:boolean=false;
+  condicion2:boolean=false;
+  condicion3:boolean=false;
+  condicion4:boolean=false;
+  LED1:any;
+  estado1:boolean=false;
+  LED2:any;
+  estado2:boolean=false;
+  LED3:any;
+  estado3:boolean=false;
+  LED4:any;
+  estado4:boolean=false;
+  constructor(private db:Firestore) {
+  }
+
+async estado() {
+  this.condicion = !this.condicion;
+  this.estado1=!this.estado1;
+  this.LED1 = doc(this.db,'controlLED','LED1');//RUTA DE TABLA EN LA BD
+  await setDoc(this.LED1, { encender: this.estado1});//CAMBIA EL ATRIBUTO DE LA TABLA
+  
+}
+async estad() {
+  this.condicion2 = !this.condicion2;
+  this.estado2=!this.estado2;
+  this.LED2 = doc(this.db,'controlLED','LED2');//RUTA DE TABLA EN LA BD
+  await setDoc(this.LED2, { encender: this.estado2});//CAMBIA EL ATRIBUTO DE LA TABLA
+  
+}
+async esta() {
+  this.condicion3 = !this.condicion3;
+  this.estado3=!this.estado3;
+  this.LED3 = doc(this.db,'controlLED','LED3');//RUTA DE TABLA EN LA BD
+  await setDoc(this.LED3, { encender: this.estado3});//CAMBIA EL ATRIBUTO DE LA TABLA
+  
+}
+async est() {
+  this.condicion4 = !this.condicion4;
+  this.estado4=!this.estado4;
+  this.LED4 = doc(this.db,'controlLED','LED4');//RUTA DE TABLA EN LA BD
+  await setDoc(this.LED4, { encender: this.estado4});//CAMBIA EL ATRIBUTO DE LA TABLA
+  
+}
+async apagar(){
+  this.estado1 = !this.estado1;
+  this.estado2 = !this.estado2;
+  this.estado3 = !this.estado3;
+  this.estado4 = !this.estado4;
+  
+  this.LED1 = doc(this.db,'controlLED','LED1');
+  this.LED2 = doc(this.db,'controlLED','LED2');
+  this.LED3 = doc(this.db,'controlLED','LED3');
+  this.LED4 = doc(this.db,'controlLED','LED4');
+  await setDoc(this.LED1, {encender: this.estado1});
+  await setDoc(this.LED2, {encender: this.estado2});
+  await setDoc(this.LED3, {encender: this.estado3});
+  await setDoc(this.LED4, {encender: this.estado4});
+
+  
+  this.condicion = this.estado1;
+  this.condicion2 = this.estado2;
+  this.condicion3 = this.estado3;
+  this.condicion4 = this.estado4;
+}
+
+
+}
+
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDwSQ-q_t_bh6n_gkqCu4K4dxjhEPsXqRc",
+    authDomain: "primer-ej.firebaseapp.com",
+    projectId: "primer-ej",
+    storageBucket: "primer-ej.appspot.com",
+    messagingSenderId: "142914494833",
+    appId: "1:142914494833:web:fb2cd6de3e1fc2acfd96d0"
+  }
+};
