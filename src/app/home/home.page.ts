3089a1a82e00a505c60ae761f85e0a3b2e39d808
OSIsrael <israelos067@gmail.com>
2024-02-23 10:28:39 -0500

    ya ahora si xd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,6 +22,7 @@ export class HomePage {
   estado3:boolean=false;
   LED4:any;
   estado4:boolean=false;
+  apagarTodos: boolean = false;
   constructor(private db:Firestore) {
   }
 
@@ -53,27 +54,41 @@ async est() {
   await setDoc(this.LED4, { encender: this.estado4});//CAMBIA EL ATRIBUTO DE LA TABLA
   
 }
-async apagar(){
-  this.estado1 = !this.estado1;
-  this.estado2 = !this.estado2;
-  this.estado3 = !this.estado3;
-  this.estado4 = !this.estado4;
-  
-  this.LED1 = doc(this.db,'controlLED','LED1');
-  this.LED2 = doc(this.db,'controlLED','LED2');
-  this.LED3 = doc(this.db,'controlLED','LED3');
-  this.LED4 = doc(this.db,'controlLED','LED4');
-  await setDoc(this.LED1, {encender: this.estado1});
-  await setDoc(this.LED2, {encender: this.estado2});
-  await setDoc(this.LED3, {encender: this.estado3});
-  await setDoc(this.LED4, {encender: this.estado4});
+async apagar() {
+  // Si es la primera vez que se presiona o si ya estaban todos apagados
+  if (!this.apagarTodos) {
+    this.estado1 = true;
+    this.estado2 = true;
+    this.estado3 = true;
+    this.estado4 = true;
+  } else {
+    this.estado1 = false;
+    this.estado2 = false;
+    this.estado3 = false;
+    this.estado4 = false;
+  }
 
+  // Cambiar el estado de apagarTodos
+  this.apagarTodos = !this.apagarTodos;
+
+  // Actualizar los documentos en Firestore
+  this.LED1 = doc(this.db, 'controlLED', 'LED1');
+  this.LED2 = doc(this.db, 'controlLED', 'LED2');
+  this.LED3 = doc(this.db, 'controlLED', 'LED3');
+  this.LED4 = doc(this.db, 'controlLED', 'LED4');
   
+  await setDoc(this.LED1, { encender: this.estado1 });
+  await setDoc(this.LED2, { encender: this.estado2 });
+  await setDoc(this.LED3, { encender: this.estado3 });
+  await setDoc(this.LED4, { encender: this.estado4 });
+
+  // Actualizar manualmente el estado de los botones
   this.condicion = this.estado1;
   this.condicion2 = this.estado2;
   this.condicion3 = this.estado3;
   this.condicion4 = this.estado4;
 }
+
 
 
 }
